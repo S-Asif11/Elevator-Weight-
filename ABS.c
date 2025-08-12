936b070ebcd5dfb8a264be055a9f8890eaa8be1b
S-Asif11 <syedasif24257@gmail.com>
2025-08-12 18:46:25 +0530

    Create ABS.c

---
+++ ABS.c
@@ -0,0 +1,20 @@
+//abs value
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+    
+    int a;
+    
+    printf("Enter a number ");
+    scanf("%d",&a);
+    
+    //if (a <0)a=-a;
+    //printf("abs value is %d",a);
+    printf("abs value is %d",abs(a));
+    
+    
+  
+
+    return 0;
+}
