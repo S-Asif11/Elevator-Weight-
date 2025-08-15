0a0f3962a91f09ecdf3905bf7939d52e92ba1378
S-Asif11 <syedasif24257@gmail.com>
2025-08-15 12:36:28 +0530

    Update BonusEligibility.c

--- BonusEligibility.c
+++ BonusEligibility.c
@@ -3,16 +3,16 @@ int main(){
 	int years;
 	scanf("%d",&years);
 	if(years<2){
-		printf("not eligible for a bonus.");
+		printf("Not Eligible ");
 	}
 	else if (years>=2 && years<=4){
 		printf("eligible for a 10%% bonus.");
 	}
 	else if (years>=5 && years <=9){
-		printf("eligible for a 15%% bonus.");
+		printf("15%% bonus");
 	}
 	else if(years>=10){
-		printf("eligible for a 20%% bonus.");
+		printf("20%% bonus");
 	}
 	else printf(" ");
 	
