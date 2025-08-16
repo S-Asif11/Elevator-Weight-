d4ba2fe8d9878c6ae43f5809d3a4271a2366d10f
S-Asif11 <syedasif24257@gmail.com>
2025-08-16 17:19:29 +0530

    Create Find_Day_Of_Week.c

---
+++ Find_Day_Of_Week.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+
+    if (num<=7){
+        if (num == 1){
+        printf("The day is Monday.");
+    }
+    else if (num == 4){
+        printf("The day is Thursday.");
+    }
+    else if (num == 7){
+        printf("The day is Sunday.");
+    }
+    else if (num == 1){
+        printf("The day is Monday.");
+    }
+    }
+    else printf("Invalid input!.");
+
+    
+
+}
